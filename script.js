97d8e5c5683cfcb1b3657e0a83f206b1339bae5f
indrish38 <indrishsundar@gmail.com>
2023-02-06 19:09:28 +0530

    new

--- script.js
+++ script.js
@@ -21,43 +21,30 @@ form.addEventListener('submit', (event) => {
   const targetCountry = targetCountryInput.value;
   const searchQuery = searchQueryInput.value;
 
-  // Make the API request
+//   // Make the API request
   const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&regionCode=${targetCountry}&q=${searchQuery}&type=video&key=${apiKey}&maxResults=${maxResults}`;
 
+  // Get the data from the API
   axios.get(url)
     .then(response => {
       const videoData = response.data.items;
+      // fs.writeFileSync('video-data.json', JSON.stringify(videoData));
+      // console.log('Video data saved as JSON file');      
       console.log(videoData);
       displayVideos(videoData);
+      // for (const video of videoData) {
+      //   const vidurl = `https://www.googleapis.com/youtube/v3/videos?part=contentDetails,snippet,statistics&id=${video.id.videoId}&key=${apiKey}`;
+      //   axios.get(vidurl).then(response => {
+      //     const videoData1 = response.data.items;
+      //     console.log(videoData1);
+      // //     fs.writeFileSync(`video-data of ${video.id.videoId}.json`, JSON.stringify(videoData1));
+      // //     console.log('Video data saved as JSON file' + video.id.videoId);      
+      //   })
+      // }
     })
-    .catch(error => {
-      console.log(error);
-    });
 });
 
-// form.addEventListener('submit', (event) => {
-//   event.preventDefault();
 
-//   // Get the target country and the search query from the form
-//   // const targetCountry = targetCountryInput.value;
-//   const searchQuery = searchQueryInput.value;
-
-// // console.log(searchQuery);
-
-// // Make the API request
-// const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&regionCode=${targetCountry}&q=${searchQuery}&key=${apiKey}&maxResults=${maxResults}`;
-
-//   // Get the data from the API
-//   // function 
-//   axios.get(url).then(response => {
-//     videoData = response.data.items;
-//     // console.log(videoData);
-//     // fs.writeFileSync('video-data.json', JSON.stringify(videoData));
-//     // console.log('Video data saved as JSON file');
-//     // handleData(videoData);
-//     displayVideos(videoData);
-//   });
-// });
 
 // exp.get('/',(req,res)=>{
 //   res.sendFile(__dirname+"/index.html");
@@ -65,23 +52,24 @@ form.addEventListener('submit', (event) => {
 
 // // Start the server
 // exp.listen(3000, () => {
-//   console.log(`Server is listening on port ${port}`);
+//   console.log(`Server is listening on port ${3000}`);
 // });
 
   function displayVideos(data) {
     // const parentContainer = document.querySelector('#parent-container');
     // parentContainer.style.display = 'flex'; 
     // Loop through the video data and create elements for each piece of information
+
     for (const video of data) {
       const thumbnail = document.createElement('img');
-      thumbnail.src = video.snippet.thumbnails.medium.url;
-      // thumbnail.style.float='left';
+      // thumbnail.src = video.snippet.thumbnails.medium.url;
+      thumbnail.src = `https://i.ytimg.com/vi/${video.id.videoId}/mqdefault.jpg`;
       thumbnail.style.width='300px';
 
       const text = document.createElement('p');
       text.innerHTML = "<b>"+video.snippet.title + 
-                        "</b><br><br> <b>Description: </b>" + video.snippet.description + 
-                        "<br><br> <b>Channel: </b>" + video.snippet.channelTitle;
+                        "</b><br><br> <b>Channel: </b>" + video.snippet.channelTitle + 
+                        "<br><br> <b>Description: </b>" + video.snippet.description;
       text.style.marginLeft = "10px";
       
       // const title = document.createElement('p');
@@ -94,15 +82,10 @@ form.addEventListener('submit', (event) => {
       // const channel = document.createElement('p');
       // channel.innerHTML = `<b>Channel: </b>${video.snippet.channelTitle}`;
       
-      // const videoLink = 'https://www.youtube.com/watch?v=' + video.id.videoId;
       var a = document.createElement('a');
-      // var link = document.createTextNode(videoLink);
       a.href= 'https://www.youtube.com/watch?v=' + video.id.videoId ;
       a.target="_blank";
       a.appendChild(thumbnail); 
-      
-      // const line = document.createElement('p');
-      // line.textContent = '\n';
       
       const container = document.createElement('div');
       container.style.display = "flex";
@@ -111,9 +94,6 @@ form.addEventListener('submit', (event) => {
       container.appendChild(text);
       
       document.body.appendChild(container);
-
-      // const views = document.createElement('p');
-      // views.textContent = `Views: ${video.views}`;
       
       // parentContainer.appendChild(thumbnail);
       // parentContainer.appendChild(a);
@@ -128,38 +108,24 @@ form.addEventListener('submit', (event) => {
         // parentContainer.appendChild(lineBreak);
         // parentContainer.appendChild(lineBreak);
         // parentContainer.appendChild(lineBreak);
-      }    
+      // }    
 
-    // const videoList = document.getElementById('video-list');
-    // for (const video of data) {
-    //   const videoTitle = video.snippet.title;
-    //   const videoLink = 'https://www.youtube.com/watch?v=' + video.id.videoId;
-    //   var a = document.createElement('a');
-    //   var link = document.createTextNode(videoLink);
-    //   a.appendChild(link); 
-    //   a.href=videoLink;
-    //   a.target="_blank";
-    //   const listItem = document.createElement('li');
-    //   const listItem2 = document.createElement('br');
-    //   listItem.textContent = videoTitle;
-    //   // listItem2.textContent = '<br>';
-    //   videoList.appendChild(listItem);
-    //   videoList.appendChild(a);
-    //   videoList.appendChild(listItem2);
-    //   // videoList.appendChild(a2.appendChild(document.createTextNode('<br>  httttttpp')));
-    // }
+      // const videoList = document.getElementById('video-list');
+      // for (const video of data) {
+      //   const videoTitle = video.snippet.title;
+      //   const videoLink = 'https://www.youtube.com/watch?v=' + video.id.videoId;
+      //   var a = document.createElement('a');
+      //   var link = document.createTextNode(videoLink);
+      //   a.appendChild(link); 
+      //   a.href=videoLink;
+      //   a.target="_blank";
+      //   const listItem = document.createElement('li');
+      //   const listItem2 = document.createElement('br');
+      //   listItem.textContent = videoTitle;
+      //   // listItem2.textContent = '<br>';
+      //   videoList.appendChild(listItem);
+      //   videoList.appendChild(a);
+      //   videoList.appendChild(listItem2);
+      //   // videoList.appendChild(a2.appendChild(document.createTextNode('<br>  httttttpp')));
+      }
   }
-
-function handleData(data) {
-  console.log(data);
-  console.log(data.length);
-//   // const firstVideo = videoData[0];
-//   // console.log(firstVideo.snippet.title);
-//   // console.log(firstVideo.snippet.description);
-//   // console.log(videoData[0].id.videoId);
-//   // for(let i=0;i<data.length;i++)
-//   // console.log(data[i]);
-//   document.getElementById('l1').innerHTML=data[i].snippet.title;
-}
-
-
